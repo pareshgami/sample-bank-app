48afa157ee9d4990762e6fec49e6d2739edd3d25
Paresh Gami <gamiparesh5@gmail.com>
2022-03-23 20:33:53 +0530

    Unit test case

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from 'src/environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { TransferResolver } from './transfer/transfer.resolver';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent, AddEditTransferComponent, DatePickerComponent],
@@ -23,6 +24,7 @@ import { TransferResolver } from './transfer/transfer.resolver';
     IonicModule.forRoot(),
     AppRoutingModule,
     HttpClientModule,
+    CommonModule,
     ReactiveFormsModule,
     FormsModule,
     !environment.production ? StoreDevtoolsModule.instrument() : [],
